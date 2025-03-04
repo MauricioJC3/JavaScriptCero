bd59fbdb00e4cc63ba0b213169a72677675ef9e4
MauricioJC3 <a.mauriciojc03@gmail.com>
2025-03-03 22:03:16 -0500

    class continue

---
+++ assets/js/valor-referencia.js
@@ -0,0 +1,32 @@
+
+// (...nombre_variable) los tres puntos son para pasar argumentos 
+// se le conoce como operador spread
+// cuando se utiliza dentro de parentesis es para u
+// cuando se utiliza en otro lugar es para separar los argumentos
+
+
+
+// Todos los primitives son pasados por valor
+let a = 10;
+let b = a;
+a = 20; // se le reasigna un nuevo valor a ( a )
+console.log({a, b});
+
+// en javascript todos los objetos son pasados por referencia
+let mauricio = { nombre: "Mauricio"};
+let ana      = { ...mauricio };
+ana.nombre   = "ana"; // se le reasigna un nuevo valor
+
+console.log({ mauricio, ana });
+
+// Funcion
+// parametro rest averiguar (...persona)
+const cambiarNombre = ( { ...persona } ) => {
+    persona.nombre  = 'Andres';
+    return persona;
+}
+
+let kioji = { nombre: "Kioji" };
+let jiota = cambiarNombre( kioji ); // se le reasigna un nuevo valor
+
+console.log({ kioji, jiota });
